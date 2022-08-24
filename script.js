03b10733d8d90bcf857402856b2b54925d82bfe8
Iqbal Kang <iqbal.kang.cohort225@gmail.com>
2022-08-23 21:56:57 -0700

    implemented hover

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sections = document.querySelectorAll('section');
+const links = document.querySelectorAll('.navigation__link');
 
 const colors = ['pink', 'orangered', 'green', 'tomato'];
 
@@ -11,8 +12,6 @@ const navScroll = function(entries) {
   const [entry] = entries;
   let activePage = +entry.target.className.slice(-1) - 1;
 
-  const links = document.querySelectorAll('.navigation__link');
-
   links.forEach((link, i) => {
 
     if(i === activePage) link.style.backgroundColor = colors[activePage]
@@ -23,3 +22,17 @@ const navScroll = function(entries) {
 const navObserver = new IntersectionObserver(navScroll, navOptions);
 
 sections.forEach(section => navObserver.observe(section));
+
+// ========================================================
+
+links.forEach((link, i) => {
+  
+  link.addEventListener('mouseenter', () => {
+
+    link.style.backgroundColor = colors[i]
+  })
+
+  link.addEventListener('mouseleave', () => {
+    link.style.backgroundColor = "transparent"
+  })
+})
